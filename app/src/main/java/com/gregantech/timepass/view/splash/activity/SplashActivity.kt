7b7121b977754661e4fa0f83d7a267f6cc37c888
Periya <periyanayagam.s@cognitiveclouds.com>
2021-06-15 15:37:37 +0530

    Live enabled status dynamically configured with API

--- app/src/main/java/com/gregantech/timepass/view/splash/activity/SplashActivity.kt
+++ app/src/main/java/com/gregantech/timepass/view/splash/activity/SplashActivity.kt
@@ -10,21 +10,32 @@ import com.gregantech.timepass.R
 import com.gregantech.timepass.base.TimePassBaseActivity
 import com.gregantech.timepass.base.TimePassBaseResult
 import com.gregantech.timepass.network.repository.AdvertisementRepository
+import com.gregantech.timepass.network.repository.LoginRepository
 import com.gregantech.timepass.util.AdvertisementHandler
 import com.gregantech.timepass.util.constant.VIEW_MODEL_IN_ACCESSIBLE_MESSAGE
 import com.gregantech.timepass.util.sharedpreference.SharedPreferenceHelper
 import com.gregantech.timepass.view.home.activity.HomeActivity
 import com.gregantech.timepass.view.login.activity.LoginActivity
+import com.gregantech.timepass.view.login.viewmodel.LoginViewModel
 import com.gregantech.timepass.view.splash.viewmodel.AdvertisementViewModel
 
 class SplashActivity : TimePassBaseActivity() {
 
     private lateinit var viewModelFactory: AdvertisementViewModel.Factory
+    private lateinit var loginViewModelFactory: LoginViewModel.Factory
+
     private val viewModel: AdvertisementViewModel by lazy {
         requireNotNull(this) {
             VIEW_MODEL_IN_ACCESSIBLE_MESSAGE
         }
         ViewModelProvider(this, viewModelFactory).get(AdvertisementViewModel::class.java)
+    }
+
+    private val loginViewModel: LoginViewModel by lazy {
+        requireNotNull(this) {
+            VIEW_MODEL_IN_ACCESSIBLE_MESSAGE
+        }
+        ViewModelProvider(this, loginViewModelFactory).get(LoginViewModel::class.java)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,23 +50,41 @@ class SplashActivity : TimePassBaseActivity() {
             when (result.status) {
                 TimePassBaseResult.Status.SUCCESS -> {
                     AdvertisementHandler.advertisementResponse = result.data
-                    SharedPreferenceHelper.setLiveEnabled(true) //dynamic
-                    Handler().postDelayed({ navigateNextScreen() }, 0)
+                    if (SharedPreferenceHelper.isUserLoggedIn())
+                        doFetchLiveStatus()
+                    else
+                        Handler().post { navigateNextScreen() }
                 }
                 TimePassBaseResult.Status.LOADING -> {
                 }
-                TimePassBaseResult.Status.ERROR -> Handler().postDelayed(
-                    { navigateNextScreen() },
-                    0
-                )
+                TimePassBaseResult.Status.ERROR -> Handler().post { navigateNextScreen() }
             }
         })
+    }
+
+    private fun doFetchLiveStatus() {
+        loginViewModel.fetchLogin(SharedPreferenceHelper.getUserMobileNumber())
+            .observe(this, Observer {
+                when (it.status) {
+                    TimePassBaseResult.Status.SUCCESS -> {
+                        dismissProgressBar()
+                        it.data?.user?.let { user ->
+                            SharedPreferenceHelper.setUserData(user)
+                            Handler().post { navigateNextScreen() }
+                        }
+                    }
+                    TimePassBaseResult.Status.LOADING -> {
+                    }
+                    TimePassBaseResult.Status.ERROR -> Handler().post { navigateNextScreen() }
+                }
+            })
     }
 
     private fun setupViewModelFactory() {
         viewModelFactory = AdvertisementViewModel.Factory(
             AdvertisementRepository()
         )
+        loginViewModelFactory = LoginViewModel.Factory(LoginRepository())
     }
 
     private fun navigateNextScreen() {
