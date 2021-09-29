5651351929814656773f19c78eeeca3e4018d0ef
Saravanan Anand <>
2021-09-29 17:19:41 +0800

    Increased version code added null to emoptystring

--- app/src/main/java/com/gregantech/timepass/network/response/comments/Comment.kt
+++ app/src/main/java/com/gregantech/timepass/network/response/comments/Comment.kt
@@ -1,6 +1,7 @@
 package com.gregantech.timepass.network.response.comments
 
 import android.os.Parcelable
+import com.gregantech.timepass.network.jsonadapter.NullToEmptyString
 import com.squareup.moshi.JsonClass
 import kotlinx.android.parcel.Parcelize
 
@@ -8,9 +9,14 @@ import kotlinx.android.parcel.Parcelize
 @JsonClass(generateAdapter = true)
 data class Comment(
     val DateTime: String,
+    @NullToEmptyString
     val Id: String,
+    @NullToEmptyString
     val commentprofileImage: String,
+    @NullToEmptyString
     val comments: String,
+    @NullToEmptyString
     val commentuserID: String,
+    @NullToEmptyString
     val commentuserName: String
 ) : Parcelable
